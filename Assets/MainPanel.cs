e0c5d2cffc626b1c3bb0c24ebdfa2310e14242d8
Glen <glen.don.mcmanus@gmail.com>
2019-06-27 21:47:16 -0700

    origin

---
+++ Assets/MainPanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour
+{
+    public Vector2 anchorFull;
+    public Vector2 anchorFolded;
+
+    public RectTransform rect;
+}

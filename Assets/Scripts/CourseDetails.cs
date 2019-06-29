4aaf7a69994b0bbefc645ab49aa3539988475108
Glen <glen.don.mcmanus@gmail.com>
2019-06-29 12:26:53 -0700

    add CourseDetails

---
+++ Assets/Scripts/CourseDetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CourseDetails : MonoBehaviour
+{
+    public RectTransform rect;
+    public Semester semester = Semester.Fall;
+    public Text semesterText;
+    public Text courseText;
+
+    public void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        semesterText.text = semester.ToString();
+    }
+
+    public void SetDetails(Course course, RectTransform trigger)
+    {
+        rect.position = trigger.position;
+        courseText.text = course.department + " " + course.number;
+    }
+}
--- Assets/Scripts/HoverDetails.cs
+++ Assets/Scripts/HoverDetails.cs
@@ -4,26 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class HoverDetails : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverDetails : CourseDetails, IPointerEnterHandler, IPointerExitHandler
 {
     public bool hasFocus;
-    public RectTransform rect;
-    Semester semester = Semester.Fall;
-    public Text semesterText;
-    public Text courseText;
-
-    private void Awake()
-    {
-        rect = GetComponent<RectTransform>();
-        semesterText.text = semester.ToString();
-    }
-
-    public void SetDetails(Course course, RectTransform trigger)
-    {
-        rect.position = trigger.position;
-        courseText.text = course.department + " " + course.number;
-    }
-
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         hasFocus = true;

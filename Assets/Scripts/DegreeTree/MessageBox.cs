76556144c99ca7a831d2afc9e172c68ab7c792d2
Vigoroso <dylanfreddi@shaw.ca>
2019-07-06 18:15:37 -0700

    warning box prefab + cs file

---
+++ Assets/Scripts/DegreeTree/MessageBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageBox : MonoBehaviour
+{
+    public string titleContent;
+    public string messageContent;
+
+    public GameObject title;
+    public GameObject message;
+
+    public GameObject yesButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        title.GetComponent<Text>().text = titleContent;
+        message.GetComponent<Text>().text = messageContent;
+    }
+}
